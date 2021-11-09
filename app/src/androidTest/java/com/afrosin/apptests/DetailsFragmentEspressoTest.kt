aeb1ac74e0eede3eaf3d88ee80f967311b5028f5
Alexey <a.afrosin@tarifer.ru>
2021-11-09 23:53:12 +0400

    Урок 7. Задания

--- app/src/androidTest/java/com/afrosin/apptests/DetailsFragmentEspressoTest.kt
+++ app/src/androidTest/java/com/afrosin/apptests/DetailsFragmentEspressoTest.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.testing.FragmentScenario
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.lifecycle.Lifecycle
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
@@ -17,24 +18,35 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class DetailsFragmentEspressoTest {
 
-//    private lateinit var scenario: FragmentScenario<DetailsFragment>
+    private lateinit var scenario: FragmentScenario<DetailsFragment>
 
-//    @Before
-//    fun setUp() {
-//        scenario = launchFragmentInContainer()
-//    }
+    @Before
+    fun setUp() {
+        scenario = launchFragmentInContainer()
+    }
 
     @Test
     fun fragment_testBundle() {
-        //Можно передавать аргументы во Фрагмент, но это необязательно
-        val fragmentArgs = bundleOf("TOTAL_COUNT_EXTRA" to 10)
-        //Запускаем Fragment с аргументами
-        val scenario = launchFragmentInContainer<DetailsFragment>(fragmentArgs)
-        //Возможность менять стейт Фрагмента
-        scenario.moveToState(Lifecycle.State.RESUMED)
 
+        val fragmentArgs = bundleOf("TOTAL_COUNT_EXTRA" to 10)
+        val scenario = launchFragmentInContainer<DetailsFragment>(fragmentArgs)
+        scenario.moveToState(Lifecycle.State.RESUMED)
+        onView(withId(R.id.totalCountTextView)).check(matches(withText("Number of results: 10")))
+    }
+
+    @Test
+    fun fragment_testSetCountMethod() {
+        scenario.onFragment { fragment ->
+            fragment.setCount(10)
+        }
         val assertion = matches(withText("Number of results: 10"))
         onView(withId(R.id.totalCountTextView)).check(assertion)
+    }
+
+    @Test
+    fun fragment_testIncrementButton() {
+        onView(withId(R.id.incrementButton)).perform(ViewActions.click())
+        onView(withId(R.id.totalCountTextView)).check(matches(withText(TEST_NUMBER_OF_RESULTS_PLUS_1)))
     }
 
 
